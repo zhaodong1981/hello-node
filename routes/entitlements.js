917124b3da70dfb536b584c31d07f0d387ec4269
Dong Zhao <dzhao@redhat.com>
2017-04-12 10:17:04 +0800

    sample code for using /rs/users/current

--- routes/entitlements.js
+++ routes/entitlements.js
@@ -7,6 +7,7 @@ var euri = "https://api.access.redhat.com/rs/entitlements";
 var euri1 = "https://summitdemo:redhat100@api.access.redhat.com/rs/entitlements";
 var eurl_ci="https://summitdemo:redhat100@api.access.devgssci.devlab.phx1.redhat.com/rs/entitlements";
 var eurl_qa="https://api.access.qa.redhat.com/rs/entitlements";
+var user_url = "https://api.access.redhat.com/rs/users/current";
 var noEntitlements="rs/entitlements/null";
 var cookie    = require('cookie');
 var fs = require('fs');
@@ -17,16 +18,16 @@ fs.readFile('cookie.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  cookies=cookie.parse(data);
-  console.log("jwt" + cookies.rh_jwt);
-  console.log("sso" + cookies.rh_sso);
+  cookies=data;
+//  console.log("jwt" + cookies.rh_jwt);
+ // console.log("sso" + cookies.rh_sso);
 });
 
 fs.readFile('jwt.txt', 'utf8', function (err,data) {
   if (err) {
     return console.log(err);
   }
-  console.log(data);
+ // console.log(data);
   jwt_token=data;
 });
 
@@ -63,6 +64,7 @@ function getToken(){
 }
 
 
+
 /* GET entitlements listing. */
  router.get("/", function (req, res, next) {
  //request = request.defaults({jar: true});
@@ -73,7 +75,7 @@ function getToken(){
      console.log("a string");
 }
    request({
-        uri: eurl_qa,
+        uri: user_url,
         method: "GET",
         timeout: 10000,
         followRedirect: true,
@@ -85,15 +87,22 @@ function getToken(){
             'Accept': 'application/json, application/xml, */*'
         }
     }, function(error, response, body) {
+        console.log(body);
+   //     console.log(error);
+ //       console.log(response);
         if (body==null || body.startsWith("401 Unauthorized")){
             console.log("Failed to get entitlement data");
             res.send("Failed to get entitlement data\n" + body);
-        }else if(body.indexOf(noEntitlements)!=-1){
-             console.log("No entitlements");
-             res.send("<strong>You have no entitlements</strong>"+body);
         }else {
-             console.log("Valid entitlements found");
-             res.send("<strong>You have entitlements</strong><br>"+body);
+           var userInfo = JSON.parse(body);
+           console.log(userInfo);
+           if(true === userInfo.is_entitled){
+               console.log("entiled!");
+                res.send("Entitled");
+           }else{
+               console.log("not entitled");
+               res.send("Not entitled!");
+           }
         }
     });
     
